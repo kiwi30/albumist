9f528cfc978f0f4d237daf3d58d0ad81c0aa9ac1
kiwi30 <rkiwi30@gmail.com>
2016-11-09 23:58:19 +0200

    34

--- index.php
+++ index.php
@@ -18,7 +18,7 @@ if(isset($_GET['lang'])){
 
 
 putenv("LC_ALL=$language");
-setlocale(LC_ALL, $language . "UTF-8");
+setlocale(LC_ALL, $language);
 
 bindtextdomain($language, '/locale');
 textdomain($language);
